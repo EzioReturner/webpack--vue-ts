a25f49f332debf1f3bdcd12ccc472330ab9ccf9f
ezio <ezioreturner@gmail.com>
2019-08-20 09:50:31 +0800

    update

--- client/directives/highlight.js
+++ client/directives/highlight.js
@@ -2,7 +2,7 @@ import * as hljs from 'highlight.js/lib/highlight';
 import javascript from 'highlight.js/lib/languages/javascript';
 import xml from 'highlight.js/lib/languages/xml';
 hljs.registerLanguage('javascript', javascript);
-hljs.registerLanguage('htmlbars', xml);
+hljs.registerLanguage('xml', xml);
 import 'highlight.js/styles/atom-one-light.css';
 
 const hljsDirective = {};
